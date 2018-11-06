b1aafa83685640a93d52abdc9322c9c3ba5e3b2f
zubersalih <zizusalih@gmail.com>
2018-11-07 01:30:48 +0300

    Importing Project

---
+++ DumpChrome/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BrowSec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chromeExtractButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.operaExtractButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.braveExtractButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.browseAndExtractButton = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Location = new System.Drawing.Point(2, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(1198, 527);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1190, 501);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Passwords";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1184, 495);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chromeExtractButton
+            // 
+            this.chromeExtractButton.Depth = 0;
+            this.chromeExtractButton.Location = new System.Drawing.Point(1236, 89);
+            this.chromeExtractButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.chromeExtractButton.Name = "chromeExtractButton";
+            this.chromeExtractButton.Primary = true;
+            this.chromeExtractButton.Size = new System.Drawing.Size(108, 34);
+            this.chromeExtractButton.TabIndex = 2;
+            this.chromeExtractButton.Text = "Chrome";
+            this.chromeExtractButton.UseVisualStyleBackColor = true;
+            this.chromeExtractButton.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // operaExtractButton
+            // 
+            this.operaExtractButton.Depth = 0;
+            this.operaExtractButton.Location = new System.Drawing.Point(1236, 159);
+            this.operaExtractButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.operaExtractButton.Name = "operaExtractButton";
+            this.operaExtractButton.Primary = true;
+            this.operaExtractButton.Size = new System.Drawing.Size(108, 34);
+            this.operaExtractButton.TabIndex = 3;
+            this.operaExtractButton.Text = "Opera";
+            this.operaExtractButton.UseVisualStyleBackColor = true;
+            this.operaExtractButton.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // braveExtractButton
+            // 
+            this.braveExtractButton.Depth = 0;
+            this.braveExtractButton.Location = new System.Drawing.Point(1236, 238);
+            this.braveExtractButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.braveExtractButton.Name = "braveExtractButton";
+            this.braveExtractButton.Primary = true;
+            this.braveExtractButton.Size = new System.Drawing.Size(108, 34);
+            this.braveExtractButton.TabIndex = 4;
+            this.braveExtractButton.Text = "Brave";
+            this.braveExtractButton.UseVisualStyleBackColor = true;
+            this.braveExtractButton.Click += new System.EventHandler(this.materialRaisedButton3_Click);
+            // 
+            // browseAndExtractButton
+            // 
+            this.browseAndExtractButton.Depth = 0;
+            this.browseAndExtractButton.Location = new System.Drawing.Point(1236, 314);
+            this.browseAndExtractButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.browseAndExtractButton.Name = "browseAndExtractButton";
+            this.browseAndExtractButton.Primary = true;
+            this.browseAndExtractButton.Size = new System.Drawing.Size(108, 34);
+            this.browseAndExtractButton.TabIndex = 5;
+            this.browseAndExtractButton.Text = "Browse";
+            this.browseAndExtractButton.UseVisualStyleBackColor = true;
+            this.browseAndExtractButton.Click += new System.EventHandler(this.materialRaisedButton4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1381, 603);
+            this.Controls.Add(this.browseAndExtractButton);
+            this.Controls.Add(this.braveExtractButton);
+            this.Controls.Add(this.operaExtractButton);
+            this.Controls.Add(this.chromeExtractButton);
+            this.Controls.Add(this.materialTabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Sizable = false;
+            this.Text = "ChromDump";
+            this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private MaterialSkin.Controls.MaterialRaisedButton chromeExtractButton;
+        private MaterialSkin.Controls.MaterialRaisedButton operaExtractButton;
+        private MaterialSkin.Controls.MaterialRaisedButton braveExtractButton;
+        private MaterialSkin.Controls.MaterialRaisedButton browseAndExtractButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DumpChrome/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace BrowSec
+{
+    public partial class Form1 : MaterialSkin.Controls.MaterialForm
+    {
+        private static SQLiteConnection sqliteConnection;
+        private static string pathToChrome = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Google\\Chrome\\User Data\\Default\\");
+        private string pathToChromeDb = pathToChrome + "log.sqlite";
+
+        private static string pathToOpera = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Opera Software\\Opera Stable\\");
+        private string pathToOperaDb = pathToOpera + "log.sqlite";
+
+        private static string pathToBrave = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\brave\\");
+        private string pathToBraveDb = pathToBrave + "log.sqlite";
+
+        public Form1()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.BlueGrey800, MaterialSkin.Primary.BlueGrey900, MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.LightBlue200, MaterialSkin.TextShade.WHITE);
+
+ 
+        }
+
+        private void extractPasswordAndDisplay(string pathOfBrowser,string pathToDb)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ORIGIN_URL", typeof(string));
+            table.Columns.Add("ACTION_URL", typeof(string));
+            table.Columns.Add("USERNAME", typeof(string));
+            table.Columns.Add("PASSWORD_VALUE", typeof(string));
+
+            dataGridView1.DataSource = null;
+
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns[0].Width = 300;
+            dataGridView1.Columns[1].Width = 300;
+            dataGridView1.Columns[2].Width = 300;
+            dataGridView1.Columns[3].Width = 300;
+
+            File.Copy(pathOfBrowser + "Login Data", pathToDb, true);
+
+            sqliteConnection = new SQLiteConnection(@"Data Source=" + pathToDb);
+            sqliteConnection.Open();
+            SQLiteCommand command = new SQLiteCommand("select * from logins", sqliteConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+                table.Rows.Add(
+                reader["origin_url"],
+                reader["action_url"],
+                reader["username_value"],
+                Encoding.ASCII.GetString(Unprotect((byte[])reader["password_value"]))
+                );
+            sqliteConnection.Close();
+           
+        }
+
+        public static byte[] Unprotect(byte[] data)
+        {
+            try
+            {
+                //Decrypt the data using DataProtectionScope.CurrentUser.
+                return ProtectedData.Unprotect(data, null, DataProtectionScope.LocalMachine);
+            }
+            catch (CryptographicException e)
+            {
+               return null;
+            }
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            extractPasswordAndDisplay(pathToChrome,pathToChromeDb);
+        }
+
+        private void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            extractPasswordAndDisplay(pathToOpera, pathToOperaDb);
+        }
+
+        private void materialRaisedButton3_Click(object sender, EventArgs e)
+        {
+            extractPasswordAndDisplay(pathToBrave, pathToBraveDb);
+        }
+
+        private void materialRaisedButton4_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                foreach(string name in files)
+                {
+                    if(name.Contains("Login Da"))
+                    {
+                        extractPasswordAndDisplay(folderBrowserDialog1.SelectedPath+"\\", folderBrowserDialog1.SelectedPath + "\\log.sqlite");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
